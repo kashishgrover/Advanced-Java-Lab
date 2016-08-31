b332b0dbd8ee84403645f9b0f92c70c2ff94d05b
Kashish Grover <kashishgrover95@gmail.com>
2016-08-31 15:13:28 +0530

    Updated Code

--- Lab2/src/lab2/Lab2.java
+++ Lab2/src/lab2/Lab2.java
@@ -20,76 +20,140 @@ package lab2;
 
 import java.io.Console;
 import java.sql.*;
-import java.util.logging.Level;
-import java.util.logging.Logger;
 import java.util.Scanner;
 
 /**
  *
  * @author Plankton
- */
+ **/
 public class Lab2 {
 
     static final String USER = "root";
     static final String PASS = "student";
-    static final String DB_URL = "jdbc:mysql://localhost:3306/Lab2";
+    static final String DB_URL = "jdbc:mysql://localhost:3306/student";
+    
     static Connection conn;
 
-    static String sql;
     static ResultSet rs;
     static Statement stmt;
 
     static String username;
     static String password;
+    
+    static String user, pass, name = "", role = "";
+    static int age = 0;
 
     static Scanner sc = new Scanner(System.in);
     static Console console = System.console();
-
-    static String passwordRegex = "(?=^.{6,10}$)(?=.*\\d)(?=.*[a-z])(?=.*[A-Z])(?=.*[!@#$%^&amp;*()_+}{&quot;:;'?/&gt;.&lt;,])(?!.*\\s).*$";
     
-    public static void main(String[] args) {
-        try {            
+    static PreparedStatement pstmt = null;
+
+    public static void main(String[] args) {        
+
+        try {
             Class.forName("com.mysql.jdbc.Driver");
             System.out.println("Connecting to a selected database...");
             conn = DriverManager.getConnection(DB_URL, USER, PASS);
-            System.out.println("Connected database successfully...\n\n");
+            System.out.println("Connected database successfully...\n");
             
             stmt = conn.createStatement();
-            sql = "CREATE TABLE IF NOT EXISTS STUDENT("
-                    + "NAME VARCHAR(40), "
-                    + "PASSWORD VARCHAR(20), "
-                    + "AGE INT, "
-                    + "BRANCH VARCHAR(20));";
-            stmt.executeUpdate(sql);
-            sql = "CREATE TABLE IF NOT EXISTS FACULTY("
-                    + "NAME VARCHAR(40), "
-                    + "PASSWORD VARCHAR(20), "
-                    + "AGE INT, "
-                    + "BRANCH VARCHAR(20));";
-            stmt.executeUpdate(sql);
-            sql = "CREATE TABLE IF NOT EXISTS ADMIN("
-                    + "NAME VARCHAR(40), "
-                    + "PASSWORD VARCHAR(20), "
-                    + "AGE INT, "
-                    + "BRANCH VARCHAR(20));";
-            stmt.executeUpdate(sql);
-
-            System.out.print("Enter username - ");
-            username = sc.nextLine();
-            char[] passString = console.readPassword("Enter Password - ");
-            password = new String(passString);
+            pstmt = conn.prepareStatement("insert into student values(?,?,?,'Student')");
             
-            if(password.matches(passwordRegex))
-            {
-                
-            }
-            else
-            {
-                System.out.println("You are not registered, da. Go talk to Kashish.");
-            }           
-        } catch (ClassNotFoundException | SQLException ex) {
-            Logger.getLogger(Lab2.class.getName()).log(Level.SEVERE, null, ex);
+        } catch (ClassNotFoundException | SQLException e) {
+            System.out.println(e.getMessage());
         }
-    }
 
+        int ch;
+        System.out.println("Enter choice: \n1) Sign Up\n2) Sign In\n");
+        ch = sc.nextInt();
+
+        try {
+            if (ch == 1) 
+            {
+                System.out.println("Enter Username: ");
+                name = sc.next();
+                String query = "select * from student";
+                rs = stmt.executeQuery(query);
+                int flag = 0;
+                String temp;
+                while (rs.next()) 
+                {
+                    temp = rs.getString("name");
+                    if (name.equals(temp))
+                    {
+                        flag = 1;
+                        break;
+                    }
+                }
+                
+                if (flag == 1) 
+                {
+                    System.out.println("User already exists! Try signing in.");
+                }
+                
+                else 
+                {
+                    System.out.println("Password: ");
+                    pass = sc.next();
+                    System.out.println("Age: ");
+                    age = sc.nextInt();
+                    pstmt.setString(1, name);
+                    pstmt.setString(2, pass);
+                    pstmt.setInt(3, age);
+                    pstmt.execute();
+                    pstmt.close();
+                }
+                rs.close();
+            }
+        } catch (Exception e) {
+            System.out.println(e.getMessage());
+        }
+
+        try {
+            if (ch == 2) {
+                System.out.println("Username: ");
+                user = sc.next();
+                System.out.println("Password: ");
+                String passw = new String(System.console().readPassword());
+
+                String query = "select * from student";
+                try (ResultSet rs2 = stmt.executeQuery(query)) {
+                    int flag = 0;
+                    while (rs2.next()) {
+                        name = rs2.getString("name");
+                        String pwd = rs2.getString("password");
+                        if (name.equals(user) && passw.equals(pwd)) {
+                            flag = 1;
+                            age = rs2.getInt("age");
+                            role = rs2.getString("role");
+                            break;
+                        }
+                    }
+                    if (flag == 0) {
+                        System.out.println("Invalid Login!");
+                        System.exit(0);
+                    } else {
+                        System.out.println("User found!");
+                        System.out.println("Name: " + name);
+                        System.out.println("Age: " + age);
+                        System.out.println("Role: " + role);
+                        switch (role) {
+                            case "Admin":
+                                System.out.println("You can:\n1)View Table.\n2)Enter record.\n");
+                                break;
+                            case "Student":
+                                System.out.println("You can:\n1)View Table.\n");
+                                break;
+                        }
+                    }
+                }
+            }
+            stmt.close();
+            conn.close();
+        } catch (Exception e) {
+            System.out.println(e.getMessage());
+        }
+        
+        System.out.println("Goodbye!\n");
+    }
 }
