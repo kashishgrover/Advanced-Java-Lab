11b526961eb13cd330adac55def4caa40534c07d
Kashish Grover <kashishgrover95@gmail.com>
2016-08-31 14:36:16 +0530

    Created Lab3 Project

---
+++ Lab3/src/lab3/Lab3.java
@@ -0,0 +1,23 @@
+/*
+1. Develop an application for a bank. The user will have the provision to register for the application. Once registered and approved by the admin,
+the user should be able to log in using his or her credentials. Consider the scenario of not less than 10 users, and each one should be able to see
+their balance. In case of any transaction made to any other account, show the current balance in the account after the transaction. The admin should
+have the provision to block or freeze any account in case of any emergencies. The user should have a provision to see their transactions based on the
+selected dates. 
+ */
+package lab3;
+
+/**
+ *
+ * @author Plankton
+ */
+public class Lab3 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
