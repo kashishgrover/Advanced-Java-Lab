5c14a3d47eb62ef38c68fa92652514c7cdc56026
Kashish Grover <kashishgrover95@gmail.com>
2016-08-31 16:06:30 +0530

    Added Lab1 Programs

--- Lab1/src/Employee/Employee.java
+++ Lab1/src/Employee/Employee.java
@@ -10,10 +10,86 @@ After each user command, the program must print the current salary of the employ
  */
 package Employee;
 
+import java.util.Scanner;
+
 /**
  *
  * @author Plankton
  */
 public class Employee {
     
+    static String name;
+    static double salary;
+    
+    Employee(String name, double salary)
+    {
+        Employee.name = name;
+        Employee.salary = salary;
+    }
+    
+    void salaryChange(double amount)
+    {
+        Employee.salary += amount;
+    }
+    
+    void quit()
+    {
+        Employee.salary = 0.0;
+    }
+    
+    double getInfo()
+    {
+        return Employee.salary;
+    }
+}
+
+class EmployeeTest {    
+    
+    static Scanner sc = new Scanner(System.in);    
+    static int choice=0;
+    static Employee E;
+    static String name;
+    static double salary;
+    static double meow;
+    
+    public static void main(String[] args)
+    {
+	while(true)
+	{
+            System.out.println("MENU");
+            System.out.println("1. New Employee");
+            System.out.println("2. View Salary");
+            System.out.println("3. Give Increment/Decrement");
+            System.out.println("4. Fire Employee");
+            System.out.println("5. Exit");
+            System.out.print("Enter Choice: ");
+            choice=sc.nextInt();
+            switch(choice)
+            {
+                case 1: {
+                    System.out.println("Enter Name of New Employee - ");
+                    name = sc.next();
+                    System.out.println("Enter Salary of New Employee - ");
+                    salary = sc.nextDouble();
+                    E = new Employee(name,salary);
+                    break;
+                }
+                case 2: {
+                    System.out.println("Salary: "+ E.getInfo());                    
+                    break;
+                }
+                case 3: {
+                    System.out.println("Enter Increment/Decrement - ");
+                    meow = sc.nextDouble();
+                    E.salaryChange(meow);               
+                    break;
+                }
+                case 4: {
+                    E.quit();              
+                    break;
+                }
+                case 5: System.exit(0);
+            }
+        }
+    }
 }
