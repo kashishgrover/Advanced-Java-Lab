43aab4103d19f41d33adcd5d718d64fb4d83783f
Kashish Grover <kashishgrover95@gmail.com>
2016-08-31 16:09:27 +0530

    Meow

--- Lab3/src/lab3/Lab3.java
+++ Lab3/src/lab3/Lab3.java
@@ -1,9 +1,11 @@
 /*
-1. Develop an application for a bank. The user will have the provision to register for the application. Once registered and approved by the admin,
-the user should be able to log in using his or her credentials. Consider the scenario of not less than 10 users, and each one should be able to see
-their balance. In case of any transaction made to any other account, show the current balance in the account after the transaction. The admin should
-have the provision to block or freeze any account in case of any emergencies. The user should have a provision to see their transactions based on the
-selected dates. 
+ - Develop an application for a bank.
+ - The user will have the provision to register for the application.
+ - Once registered and approved by the admin,the user should be able to log in using his or her credentials.
+ - Consider the scenario of not less than 10 users, and each one should be able to see their balance.
+ - In case of any transaction made to any other account, show the current balance in the account after the transaction.
+ - The admin should have the provision to block or freeze any account in case of any emergencies.
+ - The user should have a provision to see their transactions based on the selected dates. 
  */
 package lab3;
 
