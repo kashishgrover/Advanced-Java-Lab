04da1d055e9a96811a5d5f506b8b2acf4284dfd5
Kashish Grover <kashishgrover95@gmail.com>
2016-09-13 23:56:36 +0530

    Fixed Lab3 program

--- Lab3/src/lab3/Lab3.java
+++ Lab3/src/lab3/Lab3.java
@@ -10,66 +10,207 @@
 package lab3;
 
 import java.util.Scanner;
+import java.sql.Date;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+
+import com.mysql.jdbc.Connection;
+import com.mysql.jdbc.PreparedStatement;
+import com.mysql.jdbc.Statement;
+import java.sql.SQLException;
 
 /**
  *
  * @author Plankton
  */
-class User {
-
-    String name;
-    String username;
-    String password;
-    double balance;
-
-    User(String name, String username, String password, double balance) {
-        this.name = name;
-        this.username = username;
-        this.password = password;
-        this.balance = balance;
-    }
-    
-    void addMoney() {
-        
-    }
-    
-    void withdrawMoney() {
-        
-    }            
-}
-
 public class Lab3 {
 
-    static Scanner sc = new Scanner(System.in);
-    static int choice = 0;
-    static User U;
-    static String name;
-    static double salary;
-    static double meow;
-
     public static void main(String[] args) {
-        while (true) {
-            System.out.println("MENU");
-            System.out.println("1. Login");
-            System.out.println("2. Register");
-            System.out.println("3. Exit");
-            System.out.print("Enter Choice: ");
-            choice = sc.nextInt();
-            switch (choice) {
-                case 1: {
-                    
-                    break;
-                }
-                case 2: {
-                    System.out.println("Enter Name - ");
-                    System.out.println("Enter Username - ");
-                    System.out.println("Enter Password - ");
-                    //U = new User();
-                    break;
-                }
-                case 3:
-                    System.exit(0);
-            }
+
+        Scanner scanner = new Scanner(System.in);
+        final String JDBC_DRIVER = "com.mysql.jdbc.Driver";
+        final String DB_URL = "jdbc:mysql://localhost:3306/Banking";
+        final String USER = "kashish";
+        final String PASS = "Zildjian1";
+        String user, pass, name = "", role = "";
+        int id = 0;
+        int balance = 0;
+        char app = 0;
+        Connection conn = null;
+        Statement stmt = null;
+        PreparedStatement stmt_app_reject = null;
+        PreparedStatement stmt_app_approve = null;
+        PreparedStatement make_trans = null;
+        PreparedStatement update_payee = null;
+        PreparedStatement update_payer = null;
+        PreparedStatement view_trans = null;
+        PreparedStatement pstmt = null;
+
+        try {
+            Class.forName("com.mysql.jdbc.Driver");
+            conn = (Connection) DriverManager.getConnection(DB_URL, USER, PASS);
+            stmt = (Statement) conn.createStatement();
+            pstmt = (PreparedStatement) conn.prepareStatement("insert into user values(?,?,?,?,'F')");
+            stmt_app_reject = (PreparedStatement) conn.prepareStatement("update user set approved='F' where id=?;");
+            stmt_app_approve = (PreparedStatement) conn.prepareStatement("update user set approved='Y' where id=?;");
+            make_trans = (PreparedStatement) conn.prepareStatement("insert into transaction values(?,?,?,?)");
+            update_payee = (PreparedStatement) conn.prepareStatement("update user set balance=balance+? where id=?;");
+            update_payer = (PreparedStatement) conn.prepareStatement("update user set balance=balance-? where id=?;");
+            view_trans = (PreparedStatement) conn.prepareStatement("select * from transaction where tdate between ? and ? and uid=?;");
+        } catch (ClassNotFoundException | SQLException e) {
+            System.out.println(e.getMessage());
         }
+
+        int choice;
+        System.out.println("Enter choice: \n1)Sign Up\n2)Sign In\n");
+        choice = scanner.nextInt();
+
+        try {
+            if (choice == 1) {
+                int t;
+                System.out.println("ID: ");
+                t = scanner.nextInt();
+                String query = "select * from user";
+                ResultSet rs = stmt.executeQuery(query);
+                int flag = 0;
+                while (rs.next()) {
+                    if (t == (rs.getInt("id"))) {
+                        flag = 1;
+                        break;
+                    }
+                }
+                if (flag == 1) {
+                    System.out.println("ID already exists! Try signing in.");
+                    System.exit(0);
+                } else {
+                    System.out.println("Name: ");
+                    name = scanner.next();
+                    System.out.println("Password: ");
+                    pass = scanner.next();
+                    System.out.println("ID: ");
+                    id = scanner.nextInt();
+                    System.out.println("Balance: ");
+                    int k = scanner.nextInt();
+                    pstmt.setString(1, name);
+                    pstmt.setString(2, pass);
+                    pstmt.setInt(3, id);
+                    pstmt.setInt(4, k);
+                    pstmt.execute();
+                    pstmt.close();
+                }
+                rs.close();
+            }
+
+            if (choice == 2) {
+                System.out.println("Username: ");
+                user = scanner.next();
+                System.out.println("Password: ");
+                pass = scanner.next();
+                String query = "select * from user;";
+                ResultSet rs = stmt.executeQuery(query);
+                int flag = 0;
+                while (rs.next()) {
+                    name = rs.getString("name");
+                    String password = rs.getString("password");
+                    if (name.equals(user) && pass.equals(password)) {
+                        flag = 1;
+                        id = rs.getInt("id");
+                        balance = rs.getInt("balance");
+                        app = rs.getString("approved").charAt(0);
+                        break;
+                    }
+                }
+                if (flag == 0) {
+                    System.out.println("Invalid Login!");
+                    System.exit(0);
+                } else if (app == 'F') {
+                    System.out.println("Your account is not approved.");
+                    System.exit(0);
+                } else {
+                    System.out.println("User found!");
+                    System.out.println("Name: " + name);
+                    System.out.println("ID: " + id);
+                    System.out.println("Balance: " + balance);
+                    if (name.equals("Kashish")) {
+                        int k;
+                        System.out.println("You can:\n1)Approve user.\n2)Block user.\n");
+                        k = scanner.nextInt();
+                        int uid;
+                        switch (k) {
+                            case 1:
+                                System.out.println("Enter user id: ");
+                                uid = scanner.nextInt();
+                                stmt_app_approve.setInt(1, uid);
+                                System.out.println("User approved!");
+                                stmt_app_approve.execute();
+                                break;
+                            case 2:
+                                System.out.println("Enter user id: ");
+                                uid = scanner.nextInt();
+                                stmt_app_reject.setInt(1, uid);
+                                System.out.println("User blocked!");
+                                stmt_app_reject.execute();
+                                break;
+                        }
+                    } else {
+                        System.out.println("You can:\n1)Make transaction.\n2)View transactions\n");
+                        int k;
+                        k = scanner.nextInt();
+                        switch (k) {
+                            case 1:
+                                System.out.println("Enter t_id: ");
+                                int tid = scanner.nextInt();
+                                System.out.println("Enter payee id: ");
+                                int pid = scanner.nextInt();
+
+                                make_trans.setInt(1, tid);
+                                make_trans.setInt(2, id);
+                                System.out.println("Enter amount: ");
+                                int amt = scanner.nextInt();
+                                make_trans.setInt(3, amt);
+                                System.out.println("Enter date: ");
+                                String date = scanner.next();
+                                Date d = Date.valueOf(date);
+                                make_trans.setDate(4, d);
+                                make_trans.execute();
+                                update_payee.setInt(1, amt);
+                                update_payee.setInt(2, pid);
+                                update_payee.execute();
+                                update_payer.setInt(1, amt);
+                                update_payer.setInt(2, id);
+                                update_payer.execute();
+                                System.out.println("Trnasaction made!");
+                                break;
+                            case 2:
+                                System.out.println("Enter beginning date: ");
+                                String date_beg = scanner.next();
+                                Date db = Date.valueOf(date_beg);
+                                System.out.println("Enter ending date: ");
+                                String date_end = scanner.next();
+                                Date de = Date.valueOf(date_end);
+                                view_trans.setDate(1, db);
+                                view_trans.setDate(2, de);
+                                view_trans.setInt(3, id);
+                                ResultSet rss = view_trans.executeQuery();
+                                while (rss.next()) {
+                                    System.out.println("T_ID: " + rss.getInt("tid") + ", U_ID: " + rss.getInt("uid") + ", amount: " + rss.getInt("amount") + ", date: " + rss.getDate("tdate") + "\n");
+                                }
+                                break;
+                        }
+                    }
+                }
+                rs.close();
+                stmt.close();
+                update_payee.close();
+                update_payer.close();
+                view_trans.close();
+                stmt_app_approve.close();
+                stmt_app_reject.close();
+                conn.close();
+            }
+        } catch (Exception e) {
+            System.out.println(e.getMessage());
+        }
+        System.out.println("\nThankyou for using this software!");
     }
 }
