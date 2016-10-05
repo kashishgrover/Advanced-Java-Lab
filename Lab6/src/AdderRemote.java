5cadf82dcf898e34585cce53a94a393840b6e1c3
Kashish Grover <kashishgrover95@gmail.com>
2016-10-05 15:02:42 +0530

    Cleaned up

--- Lab6/src/AdderRemote.java
+++ Lab6/src/AdderRemote.java
@@ -1,53 +1,67 @@
-import java.rmi.*;  
-import java.rmi.server.*;  
 
-public class AdderRemote extends UnicastRemoteObject implements Adder{  
-	/**
-	 * 
-	 */
-	private static final long serialVersionUID = 1L;
-	AdderRemote()throws RemoteException{  
-		super();  
-	}  
-	public double add(double x,double y){
-		return x+y;
-	}
-	
-	public double subtract(double x, double y){
-		return x-y;
-	}
-	
-	public double multiply(double x, double y){
-		return x*y;
-	}
-	
-	public double divide(double x, double y){
-		if(y!=0)return x/y;
-		else return 0;
-	}
+import java.rmi.*;
+import java.rmi.server.*;
 
-	public int compare(String a, String b) throws RemoteException {
-		int x=0,y=0;
-		if(a.length()>b.length())return 1;
-		else if (a.length()<b.length())return -1;
-		else {
-			for(int i=0;i<a.length();i++){
-				x = (int)a.charAt(i);
-				y = (int)b.charAt(i);
-				if(x>y)return 1;
-				else if(x<y)return -1;
-				else if(x==y)continue;
-			}
-		}
-		return 0;
-	}
+public class AdderRemote extends UnicastRemoteObject implements Adder {
 
-	public String reverse(String a) throws RemoteException {
-		String result="";
-		/*for(int i=a.length()-1;i>=0;i++){
-			result.concat(String.valueOf(a.charAt(i)));
-		}*/
-		result = new StringBuilder(a).reverse().toString();
-		return result;
-	}  
-}  
+    /**
+     *
+     */
+    private static final long serialVersionUID = 1L;
+
+    AdderRemote() throws RemoteException {
+        super();
+    }
+
+    @Override
+    public double add(double x, double y) {
+        return x + y;
+    }
+
+    @Override
+    public double subtract(double x, double y) {
+        return x - y;
+    }
+
+    @Override
+    public double multiply(double x, double y) {
+        return x * y;
+    }
+
+    @Override
+    public double divide(double x, double y) {
+        if (y != 0) {
+            return x / y;
+        } else {
+            return 0;
+        }
+    }
+
+    @Override
+    public int compare(String a, String b) throws RemoteException {
+        int x = 0, y = 0;
+        if (a.length() > b.length()) {
+            return 1;
+        } else if (a.length() < b.length()) {
+            return -1;
+        } else {
+            for (int i = 0; i < a.length(); i++) {
+                x = (int) a.charAt(i);
+                y = (int) b.charAt(i);
+                if (x > y) {
+                    return 1;
+                } else if (x < y) {
+                    return -1;
+                }
+            }
+        }
+        return 0;
+    }
+
+    @Override
+    public String reverse(String a) throws RemoteException {
+        String result = "";
+        result = new StringBuilder(a).reverse().toString();
+        return result;
+    }
+}
--- Lab6/src/RMIServer.java
+++ Lab6/src/RMIServer.java
@@ -1,11 +1,16 @@
-import java.rmi.*;  
+
+import java.net.MalformedURLException;
+import java.rmi.*;
 import java.rmi.registry.*;
 
-public class RMIServer{  
-	public static void main(String args[]){  
-		try{  
-			Adder stub=new AdderRemote();  
-			Naming.rebind("rmi://localhost:1234/calc",stub);  
-		}catch(Exception e){System.out.println(e);}  
-	}  
-}  
+public class RMIServer {
+
+    public static void main(String args[]) {
+        try {
+            Adder stub = new AdderRemote();
+            Naming.rebind("rmi://localhost:1234/calc", stub);
+        } catch (RemoteException | MalformedURLException e) {
+            System.out.println(e);
+        }
+    }
+}
